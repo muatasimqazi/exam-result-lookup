18cce873d93c9f7e9c887920da2bf3f520038c02
Muatasim Qazi <muatasimqazi@gmail.com>
2017-07-23 03:23:54 -0700

    project setup

---
+++ static/js/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+  var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1UuEJr1ITmVAjBrQMtIkUchpl6b-42JfFbJp4QFxCS74/edit?usp=sharing#gid=0';
+  var student_name = '';
+  var father_name = '';
+  var roll_number = '';
+  var school_name = '';
+
+  $("input:first").keyup(function() {
+    student_name = $(this).val().toUpperCase();
+  }).keyup();
+
+  $("input[id][name$='father']").keyup(function() {
+    father_name = $(this).val().toUpperCase();
+  }).keyup();
+
+// roll number
+$("input[id][name$='number']").keyup(function() {
+  roll_number = $(this).val().toUpperCase();
+}).keyup();
+$("input[id][name$='school']").keyup(function() {
+  school_name = $(this).val().toUpperCase();
+}).keyup();
+
+    $("form").submit(function(){
+      var query = '';
+      if (student_name != '') {
+        query = "select A, C, D, E, F, G, H where C="+ "\"" + student_name + "\" and D="+ "\"" + father_name + "\"";
+      } else if (roll_number != '') {
+        query = "select A, C, D, E, F, G, H where A="+ roll_number;
+      } else if(school_name != '') {
+        query = "select A, C, D, E, F, G, H where G like '%"+ school_name +"%'";
+      }
+      $('#result-data').sheetrock({
+        url: mySpreadsheet,
+        query: query,
+      });
+      $('#result-data').empty();
+      return false;
+     });
+
+
+
+
+
+});
